5e594df1046f1c13a07b6b3c2789a4dbef8b351b
abhi <abhi@kali>
2024-08-24 19:16:20 +0545

    blog sucessful

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
 	theme: {
-		extend: {
-			colors: {
-				white: '#f8f9fa'
-			},
-			fontFamily: {
-				body: ['Manrope', ...defaultTheme.fontFamily.sans]
-			},
-			gridTemplateColumns: {
-				list: 'repeat(auto-fill, minmax(400px, max-content))'
-			}
-		}
+	  extend: {
+		colors: {
+		  white: '#f8f9fa',
+		},
+		fontFamily: {
+		  body: ['Manrope', ...require('tailwindcss/defaultTheme').fontFamily.sans],
+		},
+		gridTemplateColumns: {
+		  list: 'repeat(auto-fill, minmax(400px, max-content))',
+		},
+	  },
 	},
-	plugins: [require('@tailwindcss/typography')]
-}
+	plugins: [require('@tailwindcss/typography')],
+	mode: 'jit', // Ensure JIT mode is active
+  }
+  
